e886f5dcd412e43a77dadfb4af1dcc4fe2801b15
yasminek27 <yk2154@nyu.edu>
2025-10-28 22:10:04 -0400

    edited the style of the page to match design

--- front-end/src/ChangeBio.js
+++ front-end/src/ChangeBio.js
@@ -23,10 +23,8 @@ const ChangeBio = () => {
                 <button class="save-button" type="button">Save</button>
             </div>
 
-            <div className = "bottom-nav">
-                <BottomNav />
-                {menuOpen && <MenuOverlay onClose={() => setMenuOpen(false)} />}
-            </div>
+            <BottomNav />
+            {menuOpen && <MenuOverlay onClose={() => setMenuOpen(false)} />}
         </div>
     );
 };
